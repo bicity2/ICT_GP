46eedc63d5dc5ab3cabc422b89c44fcd0407c837
bicity2 <bicity2@gmail.com>
2025-05-29 13:43:07 +0900

    マイグレーションファイルの作成

---
+++ book/database/migrations/2025_05_29_025942_create_comments_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments_ratings', function (Blueprint $table) {
+            $table->id();
+            $table->string("book_id",20);
+            $table->unsignedBigInteger("user_id");
+            $table->string("comment");
+            $table->unsignedTinyInteger("rating");
+            $table->timestamps();
+
+            // リレーションシップ（外部参照制約）の設定
+            // p159
+            $table->foreign('book_id')->references('isbn')->on('books');
+            $table->foreign('user_id')->references('id')->on('members');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments_ratings');
+    }
+};
