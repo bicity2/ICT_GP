1a0d79a7a80609ce9f531952e0594a6bcfb529da
j601u12 <j601u12@example.com>
2025-05-30 15:28:31 +0900

    色んな修正

--- book/routes/web.php
+++ book/routes/web.php
@@ -4,16 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\MemberController;
+
 Route::get('/', function () {
     return view('/index');
 });
 Route::get('/db/index', [BookController::class,'index']);
+Route::get('/db/soumu',[MemberController::class,'soumu']);
+//Route::post('/db/login',[MemberController::class,'login']);
 
 Route::get('/db/add', [BookController::class,'add']);
 Route::post('/db/add2', [BookController::class,'add2']);
 
 Route::get('/db/erase', [BookController::class,'erase']);
 Route::post('/db/erase', [BookController::class,'erase']);
+Route::post('/db/erase2', [BookController::class,'erase2']);
 Route::post('/db/erase2', [BookController::class,'erase2']);
 Route::get('/db/list', [BookController::class,'list']);
 Route::get('/db/detail', [BookController::class,'detail'])->name('db.detail');
