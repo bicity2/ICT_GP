0abcad101c67a040263b9873707f828ec3dcf248
KAMIMURA SHUN <miniaturebulb470@gmail.com>
2025-05-30 13:47:42 +0900

    一覧画面＆詳細画面　開発中

--- book/resources/views/db/comment.blade.php
+++ book/resources/views/db/comment.blade.php
@@ -8,6 +8,12 @@
         rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
         crossorigin="anonymous">
+    <style>
+        body {
+            width: 800px;
+            margin: 10px auto;
+        }
+    </style>
     <title>Document</title>
     <style>
         .container {
@@ -30,13 +36,22 @@
 <body>
     <div class="container">
         <div class="left">
-            <h1>書籍詳細</h1>
-            <div>
-                <p><strong>タイトル：</strong>サンプルブック</p>
-                <p><strong>著者：</strong>山田 太郎</p>
-                <p><strong>出版社：</strong>サンプル出版社</p>
-                <p><strong>ISBN：</strong>1234567890123</p>
-            </div>
+    <h1 class="mt-4 mb-3 border-bottom pb-2">書籍詳細</h1>
+    <div class="book-details bg-light p-4 rounded shadow-sm mb-4">
+        <div class="book-title text-primary fw-bold mb-3" style="font-size:2em; padding-left:12px;">
+            サンプルブック
+        </div>
+        <p class="mb-2"><strong>著者：</strong>山田 太郎</p>
+        <p class="mb-2"><strong>出版社：</strong>サンプル出版社</p>
+        <p class="mb-2"><strong>ISBN：</strong>1234567890123</p>
+    </div>
+
+    <!-- 評価を表示＆評価をクリックでcommendページ移動する -->
+    <h2 class="h4 mt-4">評価</h2>
+    <div class="rating-box bg-warning bg-opacity-25 p-3 rounded w-25">
+        <p class="mb-0"><strong>評価：</strong>4.5</p>
+    </div>
+            <a href="#" class="btn btn-primary mt-3">コメント投稿</a>
         </div>
         <div class="right">
             <h2>コメント</h2>
--- book/routes/web.php
+++ book/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\MemberController;
 Route::get('/', function () {
     return view('/index');
 });
@@ -13,3 +15,6 @@ Route::post('/db/add2', [BookController::class,'add2']);
 Route::get('/db/erase', [BookController::class,'erase']);
 Route::post('/db/erase', [BookController::class,'erase']);
 Route::post('/db/erase2', [BookController::class,'erase2']);
+Route::get('/db/list', [BookController::class,'list']);
+Route::get('/db/detail', [BookController::class,'detail'])->name('db.detail');
+Route::post('/db/comment_input', [ReviewController::class,'comment_input']);
